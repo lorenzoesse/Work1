6607b15398947a066050249224521e76aff3cb3a
lorenzoesse <lorenzo.soricetti@outlook.it>
2018-09-13 18:02:34 +0200

    Upload #5

--- Work1/models/book.py
+++ Work1/models/book.py
@@ -3,24 +3,18 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
+
 class Book(models.Model):
     _name = "book"
 
-
     bookName = fields.Char(string="Book title", required=True)
     isbnCode = fields.Char(string="ISBN Code", required=True)
-    publishYear = fields.Date(string="Year of publish")
+    publishYear = fields.Integer(string="Year of publish")
     author = fields.Char(string="Author", required=True)
 
     loaner = fields.Many2one('res.partner', string="On loan to")
 
 # checking that the ISBN Code is unique
-#    _sql_constrains = [
-#        ('code_unique',
-#         'UNIQUE(isbnCode)',
-#         "The ISBN code must be unique"),
-#    ]
-
     @api.constrains('isbnCode')
     def check_isbn_unique(self):
         for record in self:
